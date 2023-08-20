8c64ae6b48c7a3688dc57bbe325567fde0a1125a
thelordzeus <arnav55125@gmail.com>
2023-08-21 01:45:28 +0530

    minor change

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -2,16 +2,18 @@ export default function Navbar() {
   return (
     <nav className="fixed right-4 left-4 top-8 py-2 px-4 md:right-20 md:left-20 lg:right-24 lg:left-24 z-50 flex justify-between items-center border-2 rounded-2xl backdrop-blur-md mix-blend-difference">
       <div classNameName="flex justify-center">
-        <h1 className="text-white font-title text-3xl mx-auto">Shrinker</h1>
+        <h1 className="text-white font-title text-xl sm:text-3xl mx-auto">
+          Shrinker
+        </h1>
       </div>
       <div className="text-sm md:text-lg text-white flex items-center absolute right-1/2 translate-x-1/2"></div>
       <div className="flex items-center text-white">
-        <div className="hidden md:block">
+        <div className=" md:block">
           <a
             href="https://twitter.com/0xsrmist"
-            classNameName="text-white font-title hover:underline"
+            classNameName="text-white font-title hover:underline "
           >
-            By TPHxSRMIST🚀
+            <span className="text-sm sm:text-lg">By TPHxSRMIST🚀</span>
           </a>
         </div>
       </div>
