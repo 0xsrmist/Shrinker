8f033dbc109f41eb064553ebbbac249401318325
thelordzeus <arnav55125@gmail.com>
2023-08-20 20:40:27 +0530

    minor font updation

--- app/page.js
+++ app/page.js
@@ -1,24 +1,9 @@
-import TypeWriter from "@/components/Typewriter";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  const hats = [
-    {
-      prep: "a",
-      suffix: "Web Developer",
-    },
-    {
-      prep: "a",
-      suffix: "UI/UX Designer",
-    },
-    {
-      prep: "a",
-      suffix: "Graphics Designer",
-    },
-  ];
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <h1 className="text-xl font-title text-white ">Hello guys </h1>
+      <Hero />
     </main>
   );
 }
---
+++ components/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <div>
+      <div class=" flex flex-col justify-end snap-start snap-always">
+        <h1 class="font-title text-white text-center text-4xl md:text-7xl lg:text-7xl mb-8">
+          moonlight <br /> devs
+        </h1>
+      </div>
+    </div>
+  );
+}
